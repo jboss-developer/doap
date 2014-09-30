bed7d16afacab6b8af71056b9dffe2aa1bbc4f1c
LightGuard <lightguard.jp@gmail.com>
2014-09-30 17:21:15 -0600

    Bower Foundation, cleaning, all templates in

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@
 // gulp plugins
 var gulp           = require('gulp'),
     gutil          = require('gulp-util'),
-    rimraf         = require('gulp-rimraf'),
+    del            = require('del'),
+    deploy         = require('gulp-gh-pages'),
+    uglify         = require('gulp-uglify'),
+    sourcemaps     = require('gulp-sourcemaps'),
     //jshint         = require('gulp-jshint'),
     //usemin         = require('gulp-usemin'),
     //googlecdn = require('gulp-google-cdn'),
@@ -12,19 +15,40 @@ var gulp           = require('gulp'),
     jade           = require('gulp-jade');
 
 gulp.task('clean', function (cb) {
-  return gulp.src('./dist', { read: false })
-    .pipe(rimraf({ force: true }));
+  del(['dist/**'], cb);
 });
 
-gulp.task('compile', function() {
+gulp.task('compile-jade', function() {
   gulp.src('./src/*.jade')
     .pipe(jade({}))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', function () {
-  gutil.log('Default task goes here...');
+gulp.task('compile-scripts', function() {
+  gulp.src('./src/scripts/*')
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/scripts/'));
+  gulp.src(mainBowerFiles({filter: /\.js$/i}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/scripts/'));
 });
+
+gulp.task('compile-css', function() {
+  gulp.src(mainBowerFiles({filter: /\.css$/i}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('compile', ['compile-jade','compile-css','compile-scripts'], function() {});
+
+gulp.task('deploy', ['compile'], function () {
+    gulp.src("./dist/**/*")
+        .pipe(deploy(options));
+});
+
+gulp.task('default', ['deploy'], function () {});
 
 //var testFiles = ['spec/*.js'];
 
