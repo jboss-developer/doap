ee3ed5b62211f60b6e728aeb2fb8274b40d2d333
LightGuard <lightguard.jp@gmail.com>
2014-10-13 14:14:10 -0600

    Adding next buttons

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -11,4 +11,10 @@ $("#language").zmultiselect("uncheckall");
 $('#addVersion').trigger('click');
 $('#addSpec').trigger('click');
 $('#addOnlineAccount').trigger('click');
+$('a.next.button').on('click', function(event) {
+  event.preventDefault();
+  event.stopPropagation();
 
+  var nextTab = $('li.tab-title.active').next();
+  Foundation.libs.tab.toggle_active_tab(nextTab);
+});
